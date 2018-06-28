fc3a51eb8d6a11a75d472ca85291f15379022354
Hunter Stutts <stuttskyle@gmail.com>
2018-06-27 21:05:24 -0500

    Gave up on images in jquery, moved to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,26 @@
-var targetNumber = 53;
+// var targetNumber = "";
+
+// $("#number-to-guess").text(targetNumber);
+
+//   var counter = 0;
+
+//   // Selects a random number to be shown at the start of the game
+//   // Number should be should be between 19 - 120
+//   var numberOptions = Math.floor(Math.random()*101+19);
+
+//   // For loop to create crystals for every numberOption.
+//   for (var i = 0; i < numberOptions.length; i++) {
+
+//     // For each iteration, we will create an imageCrystal
+//     // var imageCrystal = $("<img>");
+//     // imageCrystal.addClass("crystal-image");
+
+//     // // Links to images
+//     // imageCrystal.attr("src", "https://github.com/stuttskyle/unit-4-game/blob/master/assets/images/crystal-a.jpg?raw=true", "https://github.com/stuttskyle/unit-4-game/blob/master/assets/images/crystal-b.jpg?raw=true");
+
+//     // $("#crystals").append(imageCrystal);
+//   }
+
+//   $(".crystal-image").on("click", function() {
+
+//   })
